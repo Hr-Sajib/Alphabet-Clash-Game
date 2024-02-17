d2c19978ae84fa31fa41d1902d7be3d43691ef38
hrSajib <harun15-4293@diu.edu.bd>
2024-02-17 15:48:48 +0600

    Done the score and life update part

--- scripts/alphabet.js
+++ scripts/alphabet.js
@@ -7,6 +7,13 @@ function hideElementById(elementId){
 function showElementById(elementId){
     document.getElementById(elementId).classList.remove('hidden');
 }
+function BgHighlight(elementId){
+    document.getElementById(elementId).classList.add('bg-red-100','text-black');
+}
+function BgNormal(elementId){
+    document.getElementById(elementId).classList.remove('bg-red-100','text-black');
+}
+
 
 
 
@@ -15,20 +22,48 @@ function showElementById(elementId){
 function play(){
     hideElementById('homeSection');
     showElementById('gameSection');
-    const l = continueGame();
-    document.getElementById('currentLater').innerText = l;
-    BgHighlight(l);
+    continueGame();
 }
 
 function continueGame(){
     const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('')
     const index =  Math.round(Math.random()*25);
     const later = alphabets[index];
-    return later;
+    const l = later;
+    document.getElementById('currentLater').innerText = l;
+    BgHighlight(l);
 }
-   
 
 
-function BgHighlight(elementId){
-    document.getElementById(elementId).classList.add('bg-red-100','text-black');
+document.addEventListener('keyup', handleKeypressEvent);
+
+function handleKeypressEvent(event){
+    const playerPressed = event.key;
+    const expectedKey = document.getElementById('currentLater').innerText;
+    let score = parseInt(document.getElementById('scoreCount').innerText);
+    let life = parseInt(document.getElementById('lifeCount').innerText);
+
+    if(playerPressed === expectedKey){
+        continueGame();
+        BgNormal(expectedKey);
+        score+=1;
+        document.getElementById('scoreCount').innerText = score;
+    }
+    else{
+        life-=1;
+        document.getElementById('lifeCount').innerText = life;
+        if(life===0){
+            exitGame();
+        }
+    }
+}
+
+function exitGame(){
+    hideElementById('gameSection');
+    showElementById('scoreSection');
+    setFinalScore();
+}
+
+function setFinalScore(){
+    document.getElementById('finalScore').innerText = document.getElementById('scoreCount').innerText;
 }
